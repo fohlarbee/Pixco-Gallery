363cceda677112eeb1c311017be679eec9814c65
Samuel Olanrewaju <sammyola246@gmail.com>
2024-02-11 04:16:04 +0100

    Gallery pagination 90%

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,28 +2,52 @@ import fetchImages from "@/lib/fetschImages";
 import type { ImagesResults } from "@/modals/ImageModal";
 import ImageContainer from "./ImageContainer";
 import getBase64Urls from "@/lib/getBase64";
+import getPrevNextPage from "@/lib/getPrevNextPage";
+import Footer from "./Footer";
 
 type Props = {
   topic?: string | undefined
+  page?: string | undefined
 }
 
-export default async function Gallery({topic} : Props) {
-  console.log('this is topic', topic)
-    const uri = !topic ?
-     "https://api.pexels.com/v1/curated" : `https://api.pexels.com/v1/search?query=${topic}`
+export default async function Gallery({topic = "curated", page} : Props) {
+
+    let url;
+    if(topic === "curated" && page){ // browsing beyond home
+        url = `https://api.pexels.com/v1/curated?page=${page}`;
+    }
+    else if(topic === "curated") { //home
+        url = `https://api.pexels.com/v1/curated`;
+    }
+    else if(!page){ //first page of search result
+      url = `https://api.pexe ls.com/v1/search?query=${topic}`
+
+    }
+    else{
+      url = `https://api.pexels.com/v1/search?query=${topic}&page=${page}`
+    }
 
     const images: ImagesResults | undefined = await fetchImages();
-    console.log(uri)
 
-    if (!images)
+    if (!images || images.per_page === 0)
         return <h2 className="m-4 text-2xl font-bold">No images found</h2>
     
     const photosWithBlur = await getBase64Urls(images);
+
+    //calculate paginator
+    const {prevPage, nextPage} = getPrevNextPage(images);
+
+    const footerProps = {topic, page, nextPage, prevPage};
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+    <>
+    <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
             { photosWithBlur.map(photo => (
                 <ImageContainer image={photo} key={photo.id}/>
             ))}
     </section>
+    <Footer  {...footerProps}/>
+
+    
+    </>
   )
 }
--- src/app/components/ImageContainer.tsx
+++ src/app/components/ImageContainer.tsx
@@ -1,28 +1,40 @@
 import type { Photo } from '@/modals/ImageModal';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
     image: Photo
 }
 
 export default function ImageContainer({image}: Props) {
-    return(
-        <div key={image.id} className="h-56 bg-gray-200 rounded-xl relative overflow-hidden group">
-                <Image
-                src={image.src.large}
-                alt={image.alt}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                quality={100}
-                className='group-hover:opacity-30'
-                placeholder='blur'
-                blurDataURL={image.blurredDataUrl}
-                style={{
-                    objectFit:'cover',
-            
-                }}
 
-                />
+    const widthHeightRatio = image.height / image.width;
+    const galleryHeight = Math.ceil(250 * widthHeightRatio); 
+    const imageSpan = Math.ceil(galleryHeight / 10) + 1;
+    return(
+        <div key={image.id} className="w-[250px] justify-self-center"
+            style={{
+                gridRow: `span ${imageSpan}`
+            }}
+        >
+            <Link href={image.url} target='blank' className='grid place-content-center'>
+                <div className="rounded-xl overflow-hidden group">
+                        <Image
+                        src={image.src.large}
+                        alt={image.alt}
+                        width={250}
+                        height={image.height}
+                        sizes="250px"
+                        quality={100}
+                        className='group-hover:opacity-30'
+                        placeholder='blur'
+                        blurDataURL={image.blurredDataUrl}
+                        
+
+                        />
+                </div>
+            </Link>
+
 
            
         </div>
